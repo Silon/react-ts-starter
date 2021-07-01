1fba447defa8ffcfad5dbc670949f26bb77fdc15
Damian <nowakowski.damian@gmail.com>
2021-07-01 18:02:58 +0200

    🧬 Add reducer plop

--- plopfile.js
+++ plopfile.js
@@ -1,15 +1,15 @@
 module.exports = (plop) => {
-  plop.setGenerator("component", {
-    description: "Create a component",
+  plop.setGenerator('component', {
+    description: 'Create a component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "What is your component name?",
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?',
       },
     ],
     actions: (data) => {
-      const path = "src/components/";
+      const path = 'src/components/';
 
       let actions = [
         {
@@ -33,6 +33,54 @@ module.exports = (plop) => {
           templateFile: 'plop-templates/component/types.ts.hbs',
         },
       ];
+      return actions;
+    },
+  });
+  plop.setGenerator('reducer', {
+    description: 'Create redux-toolkit reducer',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your reducer name?',
+      },
+      {
+        type: 'confirm',
+        name: 'persisted',
+        massage: 'Do you want use persistor for this reducer?',
+        default: false,
+      },
+    ],
+    actions: (data) => {
+      const path = 'src/redux/';
+
+      let actions = [
+        {
+          type: 'add',
+          path: path + '{{camelCase name}}/{{camelCase name}}.slice.ts',
+          templateFile: 'plop-templates/reducer/slice.ts.hbs',
+        },
+        {
+          type: 'add',
+          path: path + '{{camelCase name}}/{{camelCase name}}.selectors.ts',
+          templateFile: 'plop-templates/reducer/selectors.ts.hbs',
+        },
+        {
+          type: 'append',
+          path: path + 'reducers.ts',
+          pattern: /(from 'redux';)/g,
+          template:
+            "import { {{#if persisted}}T{{pascalCase name}}State, {{/if}}{{camelCase name}}Reducer } from 'src/redux/{{camelCase name}}/{{camelCase name}}.slice';",
+        },
+        {
+          type: 'append',
+          path: path + 'reducers.ts',
+          pattern: /(combineReducers\({)/g,
+          template:
+            "    {{camelCase name}}: {{#if persisted}}withPersist<T{{pascalCase name}}State>('{{camelCase name}}', {{camelCase name}}Reducer){{else}}{{camelCase name}}Reducer{{/if}},",
+        },
+      ];
+
       return actions;
     },
   });
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,8 +1,7 @@
-import { AnyAction, combineReducers } from 'redux';
+import { AnyAction, Reducer, combineReducers } from 'redux';
 import { TAuthState, authReducer } from 'src/redux/auth/auth.slice';
 
 import { BrowserHistory } from 'history';
-import { Reducer } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -14,6 +13,6 @@ function withPersist<T>(key: string, reducer: Reducer<T, AnyAction>, config = {}
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const createRootReducer = (history: BrowserHistory) =>
   combineReducers({
-    router: connectRouter(history),
     auth: withPersist<TAuthState>('auth', authReducer),
+    router: connectRouter(history),
   });
